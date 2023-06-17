b96ee15ab8588b5729c634dc8bfeb7e1dab1eff7
pauloEzequiel <paulo.ezequiel@souunit.com.br>
2023-06-17 00:19:40 -0300

    Exibindo Dados obtidos da API

--- ToDoApi/app.py
+++ ToDoApi/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restful import Api
 from resources.tarefa import Tarefa,Tarefas
+from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
 api = Api(app)
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 api.add_resource(Tarefas,'/tarefas') 
 api.add_resource(Tarefa,'/tarefas/<string:tarefa_id>') 
--- ToDoFrontEnd/js/todo.js
+++ ToDoFrontEnd/js/todo.js
@@ -3,10 +3,32 @@ const taskInput = document.querySelector(".todo-list-input");
 const addTaskBtn = document.querySelector(".todo-list-add-btn"),
 taskbox = document.querySelector(".todo-list")
 
+ObterTodasAsTarefas()
 
 function salvar(){
   console.log(JSON.stringify(document.querySelector(".todo-list-input").value))
   document.querySelector(".todo-list").innerHTML += "<li><div class='form-check'><label class='form-check-label'><input class='checkbox' type='checkbox'/>" + document.querySelector(".todo-list-input").value + "<i class='input-helper'></i></label></div><div class='setting-menu settings'><i class='uil uil-pen'></i><i class='uil uil-trash'></i></div></li>"
+}
+
+function ObterTodasAsTarefas() {
+  
+axios.get(`http://127.0.0.1:5000/tarefas`)
+    .then((response) => {
+      const tarefas = response.data;
+      var lista = document.querySelector(".todo-list");
+
+      tarefas.Tarefa.forEach(tarefa => {
+        let completed = tarefa.concluido == true ? "completed" : "";
+        let checked = tarefa.concluido == true ? "checked" : "";
+        lista.innerHTML +=`<li class="${completed}"><div class='form-check'><label class='form-check-label'><input class='checkbox' type='checkbox' ${checked}/>${tarefa.descricao } ´<i class='input-helper'></i></label></div><div class='setting-menu settings'><i class='uil uil-pen'></i><i class='uil uil-trash'></i></div></li>`
+      });
+    })
+    .catch((error) => {
+      console.error(error)
+      document.querySelector(".todo-list").innerHTML = `<span>Você não possui tarefas</span>`;
+    });
+
+ 
 }
 
 (function($) {
